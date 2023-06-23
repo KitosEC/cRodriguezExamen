811d5afe47d2d74055ab1fb6c8877f64c2894aba
master <master@SMLAB108PC23_23>
2023-06-22 21:45:54 -0500

    Agregar archivos de proyecto.

---
+++ cRodriguezExamen/cRodriguezExamen/Login.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace cRodriguezExamen
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            string usuario = "estudiante2023";
+            string clave = "uisrael2023";
+            if(txtUser.Text == usuario && txtInClave.Text== clave) { Navigation.PushAsync(new Registro()); }
+            else
+            {
+                DisplayAlert("Alerta", "Usuario no registrado", "Cerrar");
+            }
+        }
+    }
+}
---
+++ cRodriguezExamen/cRodriguezExamen/Registro.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace cRodriguezExamen
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registro : ContentPage
+    {
+        string usuario;
+        public Registro(string usuario)
+        {
+            InitializeComponent();
+            lblUsuario.Text = "Usuario conectado: " + usuario;
+        }
+
+        private void Pais_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(Pais.SelectedIndex == -1)
+            {
+                DisplayAlert("Alerta", "Seleccion invalida", "Cerrar");
+            }
+            else
+            {
+                DisplayAlert("Alerta", Pais.Items[Pais.SelectedIndex], "Cerrar");
+            }
+        }
+
+        private void Ciudad_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(Ciudad.SelectedIndex == -1)
+            {
+                DisplayAlert("Alerta", "Seleccion incorrecta", "Cerrar");
+            }
+            else
+            {
+                DisplayAlert("Alerta", Ciudad.Items[Ciudad.SelectedIndex], "Cerrar");
+            }
+        }
+
+        private void btnPago_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnResumen_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
